282d968f9e6ee954f1d33d6babd7b7e14ca994fa
z137168075 <zyycy_love@126.com>
2020-03-25 17:13:42 +0800

    模板修改

---
+++ demo/detail/router.js
@@ -0,0 +1,14 @@
+/*
+ * @Author: yuanchengyong
+ * @Date: 2020-01-13 14:06:12
+ * @Last Modified by: zyycy_love@126.com
+ * @Last Modified time: 2020-01-22 11:41:39
+ */
+import Detail from "./index"
+const routes = [
+  {
+    path: "/detail/:id?",
+    component: Detail
+  }
+];
+export default routes;
